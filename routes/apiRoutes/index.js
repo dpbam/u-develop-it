56ad1e8222ed5d262cb2d3356019edc92f361b18
Derek Phelps <derekphtpt@gmail.com>
2021-07-21 13:15:38 -0600

    12.4.4 done

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,74 +1,27 @@
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
-const PORT = process.env.PORT || 3001;
-const app = express();
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('api', apiRoutes);
 
-
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: res.message });
-            // checks if anything was deleted
-        } else if (!result.affectedRows) {
-            res.json({
-                message: 'Party not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
